a07dae64072f0173d4916a0785a4b17eef75ff6e
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-08-01 16:55:17 -0500

    feat: add seats page

---
+++ src/app/pages/seats/seats.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seats',
+  imports: [],
+  templateUrl: './seats.html',
+  styleUrl: './seats.scss'
+})
+export class Seats {
+
+}
