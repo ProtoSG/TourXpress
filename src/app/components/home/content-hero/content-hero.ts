abdbb47ae4228e3015c94e3487cc7a15135abc4f
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-08-01 16:54:22 -0500

    feat: add home/content-hero

---
+++ src/app/components/home/content-hero/content-hero.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-hero',
+  imports: [],
+  templateUrl: './content-hero.html',
+  styleUrl: './content-hero.scss'
+})
+export class ContentHero {
+
+}
