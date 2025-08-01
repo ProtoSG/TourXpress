b8c197ba7cc8a7d34b373ce0219791bb6836566f
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-08-01 16:52:09 -0500

    feat: add barrel UI

---
+++ src/app/components/UI/index.ts
@@ -0,0 +1,9 @@
+export * from "./header/header";
+export * from "./main-container/main-container";
+export * from "./section-container/section-container";
+export * from "./form-search/form-search";
+export * from "./input-select/input-select";
+export * from "./primary-button/primary-button";
+export * from "./input-date/input-date";
+export * from "./footer/footer";
+
