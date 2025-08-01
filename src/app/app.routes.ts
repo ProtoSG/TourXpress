72b7962624ff495ceba71abaa7c68d87ec37db6a
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-08-01 16:57:27 -0500

    feat: update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Home, PassengerDetails, Seats, Tickets } from './pages';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: Home
+  },
+  {
+    path: "tickets",
+    component: Tickets
+  },
+  {
+    path: "seats",
+    component: Seats
+  },
+  {
+    path: "passenger-details",
+    component: PassengerDetails
+  }
+];
