8478b987f7f1ce43778e55a5685324ad52e12956
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-08-08 02:23:27 -0500

    feat: update component UI/primary-button

--- src/app/components/UI/primary-button/primary-button.ts
+++ src/app/components/UI/primary-button/primary-button.ts
@@ -1,11 +1,12 @@
-import { Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { TypeButton } from '@models/button.model';
 
 @Component({
   selector: 'app-primary-button',
   imports: [],
   templateUrl: './primary-button.html',
-  styleUrl: './primary-button.scss'
+  styleUrl: './primary-button.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PrimaryButton {
   type= input(TypeButton.BUTTON);
