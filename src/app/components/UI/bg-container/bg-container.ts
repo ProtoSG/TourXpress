15247825ba73fa4237095cc20034a8ae824c118e
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-08-08 02:22:47 -0500

    feat: add component UI/bg-container

---
+++ src/app/components/UI/bg-container/bg-container.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bg-container',
+  imports: [],
+  templateUrl: './bg-container.html',
+  styleUrl: './bg-container.scss'
+})
+export class BgContainer {
+
+}
