07590a175acb7f6d85d18723db77ae5e661f02a6
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-08-08 02:24:43 -0500

    feat: add component seats/seat-selected

---
+++ src/app/components/seats/seat-selected/seat-selected.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-seat-selected',
+  imports: [MatIconModule],
+  templateUrl: './seat-selected.html',
+  styleUrl: './seat-selected.scss'
+})
+export class SeatSelected {
+
+}
