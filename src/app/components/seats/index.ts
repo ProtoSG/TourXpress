8f3458d44ff0aa4ef27bb2b35b8ff04e289e9681
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-08-08 02:24:58 -0500

    feat: update barrel seats

---
+++ src/app/components/seats/index.ts
@@ -0,0 +1,5 @@
+export * from "./render-seats/render-seats";
+export * from "./seat-not-selected/seat-not-selected";
+export * from "./seat-selected/seat-selected";
+export * from "./seat-selector/seat-selector";
+export * from "./price-total/price-total";
