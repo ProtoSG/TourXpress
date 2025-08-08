264ecde6bc06476540478465141b3adf671fce33
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-08-08 04:09:31 -0500

    feat: update pages/passenger-details

--- src/app/pages/passenger-details/passenger-details.ts
+++ src/app/pages/passenger-details/passenger-details.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { Footer, MainContainer } from '@components/UI';
+import { HeaderPassenger, ProgressStepsPassenger, MainContentPassenger } from '@sections/passenger-details';
 
 @Component({
   selector: 'app-passenger-details',
-  imports: [],
+  imports: [
+    MainContainer,
+    HeaderPassenger,
+    ProgressStepsPassenger,
+    MainContentPassenger,
+    Footer
+  ],
   templateUrl: './passenger-details.html',
   styleUrl: './passenger-details.scss'
 })
