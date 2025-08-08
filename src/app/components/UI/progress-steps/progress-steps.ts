b537d573f31fb4c8704d9b573deda3bcf01e2a5d
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-08-08 02:23:36 -0500

    feat: add component UI/progress-steps

---
+++ src/app/components/UI/progress-steps/progress-steps.ts
@@ -0,0 +1,27 @@
+import { Location } from '@angular/common';
+import { Component, computed, inject, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-progress-steps',
+  imports: [],
+  templateUrl: './progress-steps.html',
+  styleUrl: './progress-steps.scss'
+})
+export class ProgressSteps {
+  private location = inject(Location);
+  totalSteps:number = 3;
+  progress = signal(this.getProgressFromPath());
+  progressPercentage = computed(() => (this.progress() / this.totalSteps) * 100);
+
+  private getProgressFromPath(): number {
+    const path = this.location.path().split('?')[0];
+
+    const progressMap = new Map<string, number>([
+      ['/results', 1],
+      ['/seats-selection', 2],
+      ['/passenger-details', 3],
+    ]);
+
+    return progressMap.get(path) ?? 0;
+  }
+}
