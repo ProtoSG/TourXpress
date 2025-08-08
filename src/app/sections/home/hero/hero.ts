529cf1ff23a9ed009f9364e3b1082705975ff02d
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-08-08 02:25:51 -0500

    feat: update section home/hero

--- src/app/sections/home/hero/hero.ts
+++ src/app/sections/home/hero/hero.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { ContentHero, DestinationCard } from '@components/home';
-import { FormSearch, Header, SectionContainer } from '@components/UI';
+import { BgContainer, FormSearch, Header, SectionContainer } from '@components/UI';
 
 @Component({
   selector: 'app-hero',
-  imports: [Header, SectionContainer, ContentHero, DestinationCard, FormSearch],
+  imports: [
+    Header,
+    SectionContainer,
+    ContentHero,
+    DestinationCard,
+    FormSearch,
+    BgContainer
+  ],
   templateUrl: './hero.html',
   styleUrl: './hero.scss'
 })
